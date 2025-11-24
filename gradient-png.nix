f2109f531ec0ae61e170d66484d9b060d8b4b726
rti <mail@rtti.de>
2025-11-24 23:48:37 +0000

    chore: switch bg to cyan

--- gradient-png.nix
+++ gradient-png.nix
@@ -4,7 +4,7 @@ pkgs.runCommand "gradient.png" { buildInputs = [ pkgs.imagemagick ]; } ''
     -size 3840x2160 \
     -define \
       gradient:angle=30 \
-      gradient:'#000000'-'#3366cc' \
+      gradient:'#000000'-'#1e8195' \
     -channel RGB \
     -depth 16 -dither FloydSteinberg -depth 8 \
     -gaussian-blur 0.1% \
